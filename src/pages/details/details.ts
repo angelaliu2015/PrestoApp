30317cf3e4cee731f5f0812d8b19c1726190b786
aagavin <aagavin@gmail.com>
2018-05-16 22:38:06 -0400

    removed comments

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the DetailsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -16,6 +10,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class DetailsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    console.log(navParams.get('userData'));
   }
 
   ionViewDidLoad() {
