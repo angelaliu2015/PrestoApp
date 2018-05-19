344fdb5740d1b410936c19c5e5ea49a29bd27c00
aagavin <aagavin@gmail.com>
2018-05-18 23:02:34 -0400

    added display of details

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -9,12 +9,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DetailsPage {
 
+  public userTable: UserTable = {balance: {balance: '', userTable: []}};
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     console.log(navParams.get('userData'));
+    this.userTable = navParams.get('userData');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DetailsPage');
   }
 
+}
+
+
+interface Balance {
+  balance: string;
+  userTable: string[][];
+}
+
+interface UserTable {
+  balance: Balance;
 }
